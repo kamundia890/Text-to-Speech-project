7ebc706191774fae869d47b2bbc7c935823ed1c3
kamundia <antonykamundia2@gmail.com>
2020-12-01 19:40:42 +0300

    first commit

---
+++ build/precache-manifest.9438a3e17360a88beb82e937d7781d1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d41f7a940cb490585ffa522cf40fe3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ec57aa30cd3d788059a",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "26f10587833ac314a625",
+    "url": "/static/js/2.4745584d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.4745584d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec57aa30cd3d788059a",
+    "url": "/static/js/main.4d99da98.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
